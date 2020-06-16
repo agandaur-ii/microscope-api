44636b38f4175887a83e01ac9fe23f4e478bce1a
Agandaur <cjwagstaffe@gmail.com>
2020-06-15 17:23:46 -0700

    before body image revamp

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,7 +5,7 @@ class Board < ApplicationRecord
   has_many :icons, dependent: :destroy
 
   has_ancestry
-  has_one_attached :image #maybe change to image?
+  has_one_attached :image 
 
   def get_image_url    
     url_for(self.image)  
